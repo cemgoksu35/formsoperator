f60fc056faea1396e4579b2f96d7194b1314c83a
Cem GÖKSU <127553434+cemgoksu35@users.noreply.github.com>
2024-06-02 22:26:54 +0300

    Add files via upload

---
+++ Forms Uygulamam/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_Uygulamam
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        double sayi1, sayi2;
+        double sonuc = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+
+
+            if (comboBox1.SelectedIndex==0)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if(comboBox1.SelectedIndex == 3)
+            {
+                sonuc = sayi1 / sayi2;
+            }
+
+            label1.Text = sonuc.ToString();
+
+        }
+    }
+}
